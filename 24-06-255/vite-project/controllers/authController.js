ae053115d2466cd8f103095bcbd76f2d385e940b
Yash Bhavsar <yashbhavsar175@gmail.com>
2025-06-24 17:54:22 +0530

    Today I Create Backend with database, also i write this code from that we can send data from api and that data stores in database

--- 24-06-255/vite-project/controllers/authController.js
+++ 24-06-255/vite-project/controllers/authController.js
@@ -1,13 +1,23 @@
+import {User} from '../models/user.js'
+
+
 export const signup_get = (req, res)=>{
     res.json({ message: 'signup' }); 
 }
 export const login_get = (req, res)=>{
     res.json({ message: 'Login' }); //res.render('Login')
 }
-export const signup_post = (req, res)=>{
-    res.send('new signup');
+export const signup_post = async (req, res)=>{
+    // res.send('new signup');
     const {email,password} = req.body;
-    console.table({"Email" : email, "Password" : password});
+    try {
+        const user = await User.create({email,password});
+        res.status(201).json(user)
+    } catch (error) {
+        console.log(error)
+        res.status(400).send('error, user not created')
+    }
+    // console.table({"Email" : email, "Password" : password});
 }
 export const login_post = (req, res)=>{
     const {email,password} = req.body;
