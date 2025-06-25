f8c358c537a5a2d7a8f2bcc1608a39470da62c69
Yash Bhavsar <yashbhavsar175@gmail.com>
2025-06-25 14:27:58 +0530

    bcrypt install

--- 25-06-25/24-06-25/vite-project/controllers/authController.js
+++ 25-06-25/24-06-25/vite-project/controllers/authController.js
@@ -1,11 +1,29 @@
 import {User} from '../models/user.js'
 
+// handle erroe
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = {email : '', password : ''}
+
+    if (err.code === 11000){ // bcz duplicate error key is 11000
+        errors.email = "Email is already Registered";
+        return errors;
+    }
+
+
+    if(err.message.includes('user validation failed')){
+        Object.values(err.errors).forEach(({properties})=>{
+            errors[properties.path] = properties.message;
+        });
+    }
+    return errors
+}
 
 export const signup_get = (req, res)=>{
     res.json({ message: 'signup' }); 
 }
 export const login_get = (req, res)=>{
-    res.json({ message: 'Login' }); //res.render('Login')
+    res.json({ message: 'Login' }); 
 }
 export const signup_post = async (req, res)=>{
     // res.send('new signup');
@@ -13,9 +31,9 @@ export const signup_post = async (req, res)=>{
     try {
         const user = await User.create({email,password});
         res.status(201).json(user)
-    } catch (error) {
-        console.log(error)
-        res.status(400).send('error, user not created')
+    } catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({errors});
     }
     // console.table({"Email" : email, "Password" : password});
 }
--- 25-06-25/24-06-25/vite-project/models/user.js
+++ 25-06-25/24-06-25/vite-project/models/user.js
@@ -1,17 +1,24 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
 
 const userschema = new mongoose.Schema({
     email:{
         type: String,
         required : [true,'Please Enter Your Email'],
-        unique : [true,'Email is already Registered'],
-        lowercase : true
+        unique : true,
+        lowercase : true,
+        validate : [validator.isEmail,'Please Enter Validate Email']
     },
     password:{
         type:String,
         required:[true,'Please Enter Password'],
-        minLength: [6,'Password Must be Minimum 6 Character']
+        minlength: [6,'Password Must be Minimum 6 Character']
     }
+})
+
+userschema.pre('save', function(next){
+    
+    next();
 })
 
 export const User = mongoose.model('user', userschema)
